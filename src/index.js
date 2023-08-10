772f91b84b2400ac314af97ce01099ff7683e131
Margarita Kachalko <margarita.kachalko@gmail.com>
2023-08-10 21:18:28 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let box = str;
+  
+  for(let i = 0; i < str.length / 2; i++) {
+    bracketsConfig.forEach(subArr => {
+      box = box.replace(subArr.reduce((sum, item) => sum + item, ""), "");
+    })
+  }
+
+  if(box.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+  
 }
